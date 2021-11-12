07fd8bfb3aef0782827af11c294fbddf3f206bc6
Subki Kadrawi <43809057+subkikadrawi@users.noreply.github.com>
2021-11-12 08:26:06 +0700

    login with google

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  // NgZone 
+} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent { }
+export class LoginComponent implements OnInit {
+
+  constructor(
+    // ngZone:NgZone
+  ){
+    // window['onSignIn'] = (user:any) => ngZone.run(
+    //   () => {
+    //     this.afterSignUp(user);
+    //   }
+    // )
+  }
+
+  ngOnInit():void {
+  }
+
+  // afterSignUp(googleUser:any){
+  //   this.socialAuthService = googleUser.getId()
+  // }
+
+  // logOut(googleUser:any): void {
+  //   googleUser.signOut();
+  // }
+
+}
